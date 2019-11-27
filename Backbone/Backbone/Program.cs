bf712eed7720664fd1738b32c0b18786556b368c
jac-banan <32095567+jac-banan@users.noreply.github.com>
2019-11-27 10:15:55 +0000

    A

--- Backbone/Backbone/Program.cs
+++ Backbone/Backbone/Program.cs
@@ -5,29 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using SQLDatabase;
 
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 namespace Backbone
 {
     class Program
     {
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
-=======
-            
->>>>>>> Stashed changes
-=======
-            
->>>>>>> Stashed changes
         }
     }
 }
